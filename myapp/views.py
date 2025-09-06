78714fc3a0efc8bf547b7a4e47f39f42022de26c
AKBU13 <ulrichatin@gmail.com>
2025-09-06 17:52:30 +0000

    Template folder created and first file index.html added

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def accueil(request):
-    return HttpResponse("<h1>Hey welcom to ma app 🚀</h1>")
+    return render(request,"index.html")
